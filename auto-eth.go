6a57e6cfd2879c3396a891177f9eda71845c7608
hardik-kansal <hkhardik4321@gmail.com>
2024-05-26 01:00:45 +0530

    added eth-verify

---
+++ auto-eth.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+	"github.com/hardik-kansal/gobank/ewt"
+)
+
+func WithEWTAuth(handlerFunc http.HandlerFunc, store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := GetTokenFromRequest(r)
+		token, err := validateJWT(tokenString)
+		if err != nil {
+			log.Printf("failed to validate token: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		if !token.Valid {
+			log.Println("invalid token")
+			permissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		userID := claims["userID"].(string)
+
+		_, err = store.GetUserByID(userID)
+		if err != nil {
+			log.Printf("failed to get user by id: %v", err)
+			permissionDenied(w)
+			return
+		}
+		handlerFunc(w, r)
+	}
+}
+
+func CreateEWT(secret []byte, userID int64) (string, error) {
+// create token and add address and userID
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, err
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := []byte("randomjwtsecretkey")
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secret), nil
+	})
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusUnauthorized, ErrorResponse{
+		Error: fmt.Errorf("permission denied").Error(),
+	})
+}
+func GetTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+	tokenQuery := r.URL.Query().Get("token")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	if tokenQuery != "" {
+		return tokenQuery
+	}
+
+	return ""
+}
+func createAndSetAuthCookie(userID int64, w http.ResponseWriter) (string, error) {
+	secret := []byte("randomjwtsecretkey")
+	token, err := CreateJWT(secret, userID)
+
+	if err != nil {
+		return "", err
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "Authorization",
+		Value: token,
+	})
+
+	return token, nil
+}
