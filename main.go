7e2e9baf35cc330c7b925dba81cdd0a02e5338ef
hardik-kansal <hkhardik4321@gmail.com>
2024-05-28 17:22:42 +0530

    remove sql-database

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/go-sql-driver/mysql"
-)
 func main(){
-	cfg := mysql.Config{
-		User:                 Envs.DBUser,
-		Passwd:               Envs.DBPassword,
-		Addr:                 Envs.DBAddress,
-		DBName:               Envs.DBName,
-		Net:                  "tcp",
-		AllowNativePasswords: true,
-		ParseTime:            true,
-	}
-	sqlStorage:=connection(cfg)  // db.go
-	db,err:=sqlStorage.Init()    // creating tables db.go
-	if err!=nil{
-		log.Fatal(err) // fatal terminates code if err and prints it.
-	}
-	store:=Newstore(db)          //db.go
-	server:=newserverAPI(":3001",*store)   // api.go
-	server.run()   // api.go
+	
 }
 
 // USE POSTMAN for testing and make sure to copy token from cookie 
